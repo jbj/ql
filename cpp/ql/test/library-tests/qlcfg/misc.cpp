5a3e99c096093d483c344ddb4b5e34a4d53e55c6
Jonas Jensen <jonas@semmle.com>
2018-11-20 13:40:05 +0100

    WIP: Range-based for loop support

--- cpp/ql/test/library-tests/qlcfg/misc.cpp
+++ cpp/ql/test/library-tests/qlcfg/misc.cpp
@@ -1,17 +1,30 @@
-struct HasTwoArgCtor {
-  int x;
-  HasTwoArgCtor(int a, int b);
-};
+// semmle-extractor-options: -std=c++17
 
 namespace std { typedef unsigned long size_t; }
 
 void* operator new  ( std::size_t count, void* ptr );
 
-template<typename T, typename... Args>
-void make(T *ptr, Args&&... args) {
-  ::new((void *)ptr) HasTwoArgCtor(args...);
+namespace placement_new {
+  struct HasTwoArgCtor {
+    int x;
+    HasTwoArgCtor(int a, int b);
+  };
+
+  template<typename T, typename... Args>
+  void make(T *ptr, Args&&... args) {
+    ::new((void *)ptr) HasTwoArgCtor(args...);
+  }
+
+  void make_HasTwoArgCtor(HasTwoArgCtor *p) {
+    make(p, 1, 2);
+  }
 }
 
-void make_HasTwoArgCtor(HasTwoArgCtor *p) {
-  make(p, 1, 2);
+namespace range_based_for {
+  void array() {
+    int arr[4];
+    for (auto &el : arr) {
+      el = 0;
+    }
+  }
 }
