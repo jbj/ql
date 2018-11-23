43430678497069d7facaf4524a0c18939823e30f
Jonas Jensen <jonas@semmle.com>
2018-11-23 11:31:52 +0100

    WIP: Fix nested Microsoft __try

--- cpp/ql/test/library-tests/qlcfg/ms_try_except.cpp
+++ cpp/ql/test/library-tests/qlcfg/ms_try_except.cpp
@@ -3,17 +3,22 @@ void ms_try_except(int j) {
     int x;
 
     __try {
-        x = 1;
-    }
-    __except (j) {
-        x = 2;
-    }
+        __try {
+            x = 1;
+        }
+        __except (j) {
+            x = 2;
+        }
 
-    __try {
-        x = 3;
+        __try {
+            x = 3;
+        }
+        __finally {
+            x = 4;
+        }
+    } __except (j - 1) {
+        x = 5;
     }
-    __finally {
-        x = 4;
-    }
+    ;
 }
 
