d489ce13872c88ce048423fb7940d3213d132053
Jonas Jensen <jonas@semmle.com>
2019-01-14 14:16:17 +0100

    WIP: Rename and improve test cases

--- cpp/ql/test/library-tests/constants/addresses/addresses.cpp
+++ cpp/ql/test/library-tests/constants/addresses/addresses.cpp
@@ -8,6 +8,8 @@ int int_var;
 int int_arr[4];
 int int_arr_arr[4][4];
 
+int *const const_ptr = &int_var;
+
 void sideEffect();
 using fptr = void (*)();
 using fref = void (&)();
@@ -17,39 +19,41 @@ using fref = void (&)();
 // All variables in this function are initialized to constants, as witnessed by
 // the `constexpr` annotation that compiles under C++14.
 void constantAddresses(int param) {
-    constexpr int *p1 = &int_var;
-    constexpr int *p2 = &*&int_var;
-    constexpr int *p3 = &int_arr[1] + 1;
-    constexpr int (*p4)[4] = &int_arr_arr[1] + 1;
-    constexpr int *p5 = &int_arr_arr[1][1] + 1;
-    constexpr int *p6 = &int_arr_arr[int_const][extern_int_const];
+    constexpr int *ptr_int = &int_var;
+    constexpr int *ptr_deref_chain = &*&int_var;
+    constexpr int *ptr_array = &int_arr[1] + 1;
+    constexpr int (*ptr_to_array)[4] = &int_arr_arr[1] + 1;
+    constexpr int *array2d = &int_arr_arr[1][1] + 1;
+    constexpr int *const_ints = &int_arr_arr[int_const][extern_int_const];
 
     // Commented out because clang and EDG disagree on whether this is
     // constant.
-    //constexpr int *p7 = &int_arr[ ({ 1; }) ]; // StmtExpr
+    //constexpr int *stmtexpr_int = &int_arr[ ({ 1; }) ];
 
-    constexpr int *p8 = &int_arr[ ((void)0, 1) ]; // CommaExpr
-    constexpr int *p9 = &int_arr[int_const ? 1 : param];
-    constexpr int *p10 = &int_arr[&int_arr + 1 ? 1 : param]; // recursive
-    constexpr int *p11 = &int_arr[&int_arr[1] - &int_arr[0]];
+    constexpr int *comma_int = &int_arr[ ((void)0, 1) ];
+    constexpr int *comma_addr = ((void)0, &int_var);
+    constexpr int *ternary_true = int_const ? &int_var : &param;
+    constexpr int *ternary_false = !int_const ? &param : &int_var;
+    constexpr int *ternary_ptr_cond = (&int_arr+1) ? &int_var : &param;;
+    constexpr int *ptr_subtract = &int_arr[&int_arr[1] - &int_arr[0]];
 
-    constexpr int *p12 = p11 + 1;
+    constexpr int *constexpr_va = ptr_subtract + 1;
 
-    constexpr int &r1 = int_var;
-    constexpr int &r2 = int_arr_arr[1][1];
-    constexpr int &r4 = r2;
-    constexpr int &r5 = *(&r2 + 1);
+    constexpr int &ref_int = int_var;
+    constexpr int &ref_array2d = int_arr_arr[1][1];
+    constexpr int &ref_va = ref_array2d;
+    constexpr int &ref_va_arith = *(&ref_array2d + 1);
 
-    constexpr fptr fp1 = sideEffect;
-    constexpr fptr fp2 = &sideEffect;
-    constexpr fptr fp3 = &**&**sideEffect;
-    constexpr fptr fp4 = **&**&**sideEffect;
-    constexpr fptr fp5 = *&sideEffect;
+    constexpr fptr fp_implicit = sideEffect;
+    constexpr fptr fp_explicit = &sideEffect;
+    constexpr fptr fp_chain_addressof = &**&**sideEffect;
+    constexpr fptr fp_chain_deref = **&**&**sideEffect;
+    constexpr fptr fp_shortchain_deref = *&sideEffect;
 
-    constexpr fref fr1 = sideEffect;
-    constexpr fref fr2 = *&sideEffect;
-    constexpr fref fr3 = **sideEffect;
-    constexpr fref fr4 = fr1;
+    constexpr fref fr_int = sideEffect;
+    constexpr fref fr_deref = *&sideEffect;
+    constexpr fref fr_2deref = **sideEffect;
+    constexpr fref fr_va = fr_int;
 }
 
 
@@ -57,18 +61,19 @@ void constantAddresses(int param) {
 
 // All variables in this function are initialized to non-const values. Writing
 // `const` in front of any of the variables will be a compile error (C++14).
-void nonConstantAddresses(int param, int *pparam, int &rparam, fref frparam) {
-    int *p3 = &int_arr[param];
-    int *p6 = &int_arr[extern_int_const_noinit];
+void nonConstantAddresses(const int param, int *const pparam, int &rparam, fref frparam) {
+    int *int_param = &int_arr[param];
+    int *int_noinit = &int_arr[extern_int_const_noinit];
 
-    int *p7 = &int_arr[ ({ sideEffect(); 1; }) ]; // StmtExpr
-    int *p8 = &int_arr[ (sideEffect(), 1) ]; // CommaExpr
-    int *p9 = &int_arr[int_const ? param : 1];
-    int *p10 = &int_arr[&int_arr + 1 ? param : 1]; // recursive
-    int *p12 = pparam;
+    int *side_effect_stmtexpr = &int_arr[ ({ sideEffect(); 1; }) ];
+    int *side_effect_comma_int = &int_arr[ (sideEffect(), 1) ];
+    int *side_effect_comma_addr = (sideEffect(), &int_var);
+    int *ternary_int = &int_arr[int_const ? param : 1];
+    const int *ternary_addr = int_const ? &param : &int_var;
+    int *va_non_constexpr = pparam;
 
-    int *&&r3 = &int_var; // reference to temporary is not a const
-    int &r4 = rparam;
+    int *&&ref_to_temporary = &int_var; // reference to temporary is not a const
+    int &ref_param = rparam;
 
-    fref fr4 = frparam;
+    fref fr_param = frparam;
 }
